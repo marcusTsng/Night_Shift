a8d132cef78b04ac6257bacc0de7b221de8f8ee0
Marcus Tsang <marcuskmTsang@gmail.com>
2026-03-26 11:29:47 +0800

    basic flashlight system

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,9 +6,13 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
 
+    private Vector2 _mouse_pos;
     private float _moveHor = 0;
     private float _moveVert = 0;
     private Rigidbody2D _rb;
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject _flashlight;
+    public bool flashlight_on = true;
 
     void Start()
     {
@@ -19,10 +23,23 @@ public class PlayerManager : MonoBehaviour
     {
         _moveHor = Input.GetAxis("Horizontal");
         _moveVert = Input.GetAxis("Vertical");
+        _mouse_pos = Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(0)) //toggle flashlight
+        {
+            flashlight_on = !flashlight_on;
+            _flashlight.SetActive(flashlight_on);
+        }
     }
 
     void FixedUpdate()
     {
         _rb.AddForce(new Vector2(_moveHor, _moveVert) * _speed * Time.deltaTime, ForceMode2D.Impulse);   
+
+        // Flashlight handling
+        Vector3 worldPos = cam.ScreenToWorldPoint(_mouse_pos);
+        Vector3 dir = worldPos - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
